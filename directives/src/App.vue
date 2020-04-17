58e8a9f762dedccb4e768341d2056411a711e506
Yingxiaoyang Hu <shawn.hu@mail.utoronto.ca>
2020-04-17 13:58:56 -0400

    finished directivies

--- directives/src/App.vue
+++ directives/src/App.vue
@@ -1,28 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <h1>Built-in Directives</h1>
+        <p v-text="'hello world'"></p>
+        <p v-html="'<strong>I am angry</strong>'"></p>
+        <hr />
+        <h1>Custom Directives</h1>
+        <p v-highlight:background.delayed="'red'">Color This</p>
+        <p
+            v-local-highlight:background.delayed.blink="{
+                mainColor: 'red',
+                secondColor: 'green',
+                delay: 500,
+            }"
+        >
+            Color This, Too
+        </p>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    directives: {
+        "local-highlight": {
+            bind(el, binding) {
+                var delay = 0;
+                if (binding.modifiers["delayed"]) {
+                    delay = 3000;
+                }
+                if (binding.modifiers["blink"]) {
+                    let mainColor = binding.value.mainColor;
+                    let secondColor = binding.value.secondColor;
+                    let currColor = mainColor;
+
+                    setTimeout(() => {
+                        setInterval(() => {
+                            currColor == secondColor
+                                ? (currColor = mainColor)
+                                : (currColor = secondColor);
+
+                            if (binding.arg == "background") {
+                                el.style.backgroundColor = currColor;
+                            } else {
+                                el.style.color = currColor;
+                            }
+                        }, binding.value.delay);
+                    }, delay);
+                } else {
+                    setTimeout(() => {
+                        if (binding.arg == "background") {
+                            el.style.backgroundColor = binding.value;
+                        } else {
+                            el.style.color = binding.value;
+                        }
+                    }, delay);
+                }
+            },
+        },
+    },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- directives/src/main.js
+++ directives/src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.directive("highlight", {
+    bind(el, binding) {
+        // el.style.backgroundColor = "red";
+        // el.style.backgroundColor = binding.value;
+        var delay = 0;
+        if (binding.modifiers["delayed"]) {
+            delay = 3000;
+        }
+
+        setTimeout(() => {
+            if (binding.arg == "background") {
+                el.style.backgroundColor = binding.value;
+            } else {
+                el.style.color = binding.value;
+            }
+        }, delay);
+    },
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: (h) => h(App),
+}).$mount("#app");
