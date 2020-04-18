d49caa526bd3f2c72745eb9f2c2f72160342f217
Yingxiaoyang Hu <shawn.hu@mail.utoronto.ca>
2020-04-17 23:40:33 -0400

    did some firebase testing wohoo

--- requests/src/App.vue
+++ requests/src/App.vue
@@ -1,28 +1,69 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <h1>Http</h1>
+        <hr />
+        <label>Username</label>
+        <input type="text" v-model="user.username" />
+        <hr />
+        <label>Email</label>
+        <input type="text" v-model="user.email" />
+        <hr />
+        <button @click="submit()">Submit</button>
+        <hr />
+        <button @click="fetchData">Get Data</button>
+        <ul>
+            <li v-for="u in users" :key="u">
+                {{ u.username }} - {{ u.email }}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    data() {
+        return {
+            user: {
+                username: "",
+                email: "",
+            },
+            users: [],
+        };
+    },
+    methods: {
+        submit() {
+            this.$http
+                .post(
+                    "https://vuejs-http-94841.firebaseio.com/data.json",
+                    this.user
+                )
+                .then(
+                    (response) => {
+                        console.log(response);
+                    },
+                    (error) => {
+                        console.log(error);
+                    }
+                );
+        },
+        fetchData() {
+            this.$http
+                .get("https://vuejs-http-94841.firebaseio.com/data.json")
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    const resultArray = [];
+                    console.log(data);
+                    for (let key in data) {
+                        resultArray.push(data[key]);
+                    }
+
+                    this.users = resultArray;
+                });
+        },
+    },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
